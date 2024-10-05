0b43b64b93bef3ddf2d857bda7e8cd166bca77f1
Lina <alicerodnulkina@gmail.com>
2024-10-05 17:51:49 +0200

    pizda

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,26 +3,23 @@ import SectionTwo from './page_one/section_two/section-two.jsx'
 import SectionThree from './page_one/section_three/section-three.jsx'
 import SectionFour from './page_one/section_four/section-four.jsx'
 import SectionFive from './page_one/section_five/section-five.jsx'
-import Footer from './page_one/footer/footer.jsx'
 import Container from './page_one/container_for_nav_and_header/container.jsx'
-import {Routes,Route } from 'react-router-dom'
+import SectionOne from './page_one/section_one/section-one.jsx'
 function App() {
  
 
   return (
     <>
-    <Routes>
-        <Route path='/' element = {<Container />} />
-        <Route path='/SectionTwo' element = {<SectionTwo/>} />
-    </Routes>
-
+          <SectionOne/>
           <SectionTwo/>
           <SectionThree />
           <SectionFour />
           <SectionFive/>
-          <Footer/>
     </>
+    
+    
   )
+  
 }
 
 export default App
